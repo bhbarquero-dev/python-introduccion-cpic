18fa2118b663b04a0cf11e7145885116a238a933
Bernal I. Hernández Barquero <bhbarquero@outlook.com>
2025-03-02 09:55:50 -0600

    es Par

--- semana_!/prueba_gemini.py
+++ semana_!/prueba_gemini.py
@@ -8,5 +8,15 @@ def es_primo(numero):
     print(f"{numero} es primo")
     return True
 
+# es par
+def es_par(numero):
+    if numero % 2 == 0:
+        print(f"{numero} es par")
+        return True
+    else:
+        print(f"{numero} no es par")
+        return False
 
+
+es_par(2)
 es_primo(3)
